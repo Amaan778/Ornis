dc9a204e498d87d6dc41a527a2db287264e8837e
Amaan Ansari <amaanansari778@gmail.com>
2025-05-31 22:48:15 +0530

    This is ornis app

---
+++ app/src/main/java/com/app/ornisapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.app.ornisapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btn:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+
+        }
+
+    }
+}
